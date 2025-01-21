284e0c5b322039cc5f306ec98c0d300c3ec4fd85
scarint <michaelpballard@gmail.com>
2025-01-20 16:13:23 -0800

    player movement

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 # import constants
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     # print(f"Starting asteroids!")
@@ -11,6 +13,18 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
+    # pygame groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Instantiate player object
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, 
+                    SCREEN_HEIGHT / 2, )
 
     game_loop = True
 
@@ -21,7 +35,22 @@ def main():
 
 
         screen.fill((0,0,0))
+
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        #player.draw(screen)
+        #player.update(dt)
+
+
         pygame.display.flip()
+
+
+
+
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
